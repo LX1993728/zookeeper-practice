a51c51a7e8da6d7e9a337a5a29680ff198343d03
刘勋 <xun.liu@baishancloud.com>
2019-08-08 15:47:02 +0800

    添加Curator对指定目录的递归监听(包括所有的子孙目录以及子孙目录下的所有叶子节点的变化)

---
+++ zoo-curator/src/main/java/liuxun/zoo/curator/watcher/CuratorWatcher3.java
@@ -0,0 +1,52 @@
+package liuxun.zoo.curator.watcher;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.TreeCache;
+import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
+import org.apache.curator.framework.recipes.cache.TreeCacheListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+public class CuratorWatcher3 {
+
+    /**
+     * zookeeper地址
+     */
+//	static final String CONNECT_ADDR = "192.168.1.171:2181,192.168.1.172:2181,192.168.1.173:2181";
+    static final String CONNECT_ADDR = "localhost:12181";
+    /**
+     * session超时时间
+     */
+    static final int SESSION_OUTTIME = 5000;//ms
+
+    public static void main(String[] args) throws Exception {
+
+
+        //1 重试策略：初试时间为1s 重试10次
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 10);
+        //2 通过工厂创建连接
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.builder()
+                .connectString(CONNECT_ADDR)
+                .sessionTimeoutMs(SESSION_OUTTIME)
+                .retryPolicy(retryPolicy)
+                .build();
+
+        //3 建立连接
+        curatorFramework.start();
+
+        final TreeCache treeCache = new TreeCache(curatorFramework, "/orchsym");
+        treeCache.getListenable().addListener(new TreeCacheListener() {
+            @Override
+            public void childEvent(CuratorFramework curatorFramework, TreeCacheEvent treeCacheEvent) throws Exception {
+                System.out.println(treeCacheEvent.getType());
+                System.out.println(treeCacheEvent.getData().getPath());
+                System.out.println(new String(treeCacheEvent.getData().getData()));
+            }
+        });
+        treeCache.start();
+        ;
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+}
